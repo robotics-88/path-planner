59c69523e979524fbacb5c4bdb02064503793308
Erin88 <erin@robotics88.com>
2023-12-06 18:25:15 -0500

    Added min and max alts as launch params

--- path_searching/src/kinodynamic_astar.cpp
+++ path_searching/src/kinodynamic_astar.cpp
@@ -479,6 +479,8 @@ void KinodynamicAstar::setParam(ros::NodeHandle& nh)
   nh.param("search/allocate_num", allocate_num_, -1);
   nh.param("search/check_num", check_num_, -1);
   nh.param("search/optimistic", optimistic_, true);
+  nh.param("search/min_alt", min_alt_, 1.5);
+  nh.param("search/max_alt", max_alt_, 5.0);
   tie_breaker_ = 1.0 + 1.0 / 10000;
 
   double vel_margin;
