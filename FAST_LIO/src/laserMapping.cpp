986694199fa26be354d5ec24e657ea73d7d81eee
Gus Meyer <142263265+gus-robotics88@users.noreply.github.com>
2024-01-11 09:38:15 -0800

    Publishing vision pose by default (#5)

--- FAST_LIO/src/laserMapping.cpp
+++ FAST_LIO/src/laserMapping.cpp
@@ -648,9 +648,8 @@ int main(int argc, char** argv)
     ros::Publisher pubPath          = nh.advertise<nav_msgs::Path> 
             ("/path", 10);
     ros::Publisher pose_publisher = nh.advertise<geometry_msgs::PoseStamped>(slam_pose_topic, 10);
-#ifdef DEPLOY
     ros::Publisher mavros_pose_publisher = nh.advertise<geometry_msgs::PoseStamped>("/mavros/vision_pose/pose", 10);
-#endif
+    
     geometry_msgs::PoseStamped msg_body_pose;
     nav_msgs::Path path;
     path.header.stamp    = ros::Time::now();
@@ -1323,9 +1322,7 @@ int main(int argc, char** argv)
             msg_body_pose.pose.orientation.w = geoQuat.w;
 
             pose_publisher.publish(msg_body_pose);
-            #ifdef DEPLOY
             mavros_pose_publisher.publish(msg_body_pose);
-            #endif
 
             /******* Publish Path ********/
             msg_body_pose.header.frame_id = slam_map_frame;
