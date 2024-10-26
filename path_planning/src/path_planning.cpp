8c5658a91572dc6c2c8e2fba2e3aaf24bc6c6052
rien88 <erin@robotics88.com>
2024-10-26 18:41:45 -0400

    Reverted QOS change, barely helped and instead will fix with composable

--- path_planning/src/path_planning.cpp
+++ path_planning/src/path_planning.cpp
@@ -460,12 +460,10 @@ int main(int argc, char **argv)
 	node->get_parameter("search/pose_topic", pose_topic_);
 	node->get_parameter("search/cloud", cloud_topic);
 
-    auto sensor_qos = rclcpp::SensorDataQoS();
-
-	auto odom_sub = node->create_subscription<nav_msgs::msg::Odometry>("/mavros/odometry/out", sensor_qos, odomCb);
-	auto pointcloud_sub = node->create_subscription<sensor_msgs::msg::PointCloud2>(cloud_topic, sensor_qos, cloudCallback);
-	auto pose_sub = node->create_subscription<geometry_msgs::msg::PoseStamped>(pose_topic_, sensor_qos, poseCb);
-	auto goal_sub = node->create_subscription<geometry_msgs::msg::PoseStamped>("/goal", sensor_qos, goalCb);
+	auto odom_sub = node->create_subscription<nav_msgs::msg::Odometry>("/mavros/odometry/out", 1, odomCb);
+	auto pointcloud_sub = node->create_subscription<sensor_msgs::msg::PointCloud2>(cloud_topic, 1, cloudCallback);
+	auto pose_sub = node->create_subscription<geometry_msgs::msg::PoseStamped>(pose_topic_, 100, poseCb);
+	auto goal_sub = node->create_subscription<geometry_msgs::msg::PoseStamped>("/goal", 10000, goalCb);
 	auto time_index_sub = node->create_subscription<std_msgs::msg::Int64>("/demo_node/trajectory_time_index", 1000, timeindexCallBack);
 
 	vis_pub = node->create_publisher<visualization_msgs::msg::Marker>("visualization_marker", 0 );
@@ -476,7 +474,7 @@ int main(int argc, char **argv)
 
 	Bspline_pub = node->create_publisher<trajectory_msgs::msg::MultiDOFJointTrajectory>("smooth_waypoints",1);
 
-	pos_pub = node->create_publisher<nav_msgs::msg::Path>("/search_node/trajectory_position", sensor_qos);
+	pos_pub = node->create_publisher<nav_msgs::msg::Path>("/search_node/trajectory_position", 1);
     vel_pub = node->create_publisher<nav_msgs::msg::Path>("/search_node/trajectory_velocity", 1);
     acc_pub = node->create_publisher<nav_msgs::msg::Path>("/search_node/trajectory_accel", 1);
     path_size_pub = node->create_publisher<std_msgs::msg::Int64>("/search_node/trajectory_path_size", 1);
